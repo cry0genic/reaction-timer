0cbabdcc967369e0eb584f04b93ecdf7c9a87b67
cry0genic <cry0genic1103@gmail.com>
2021-08-25 09:28:08 +0530

    final

---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction Time: {{score}} ms</p>
+  <p class="rank"> {{rank}}</p>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return {
+      rank: null
+    }
+  },
+  mounted() {
+    if (this.rank <250){
+      this.rank = 'Ninja fingers'
+    } else if (this.rank < 400){
+      this.rank = 'Rapid reflexes'
+    } else {
+      this.rank = 'Snail pace'
+    }
+  }
+}
+</script>
+
+<style>
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
